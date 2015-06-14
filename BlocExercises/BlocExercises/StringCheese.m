25a87189cc207b89fae75be0c5d77b0cc209321e
Chad Clayton <chad@kcwaverider.com>
2015-06-13 19:08:29 -0500

    Assignment 8 completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,36 +11,46 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSString *fullSentance = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return fullSentance;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    NSString *shortName = nil;
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
+        NSRange cheeseRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+        shortName = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
     } else {
         /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+        shortName = cheeseName;
     }
 
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
 
-    return nil;
+    return shortName;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    
+    NSString *cheeseString = nil;
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        cheeseString = @"1 cheese";
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        NSString *cheesyEnding = @" cheeses";
+        NSString *cheeseNumberString = [@(cheeseCount) stringValue];
+        cheeseString = [cheeseNumberString stringByAppendingString:cheesyEnding];
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
     
-    return nil;
+    return cheeseString;
 }
 
 @end
