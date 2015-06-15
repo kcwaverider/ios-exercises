fd589ac20f075da9e88668c30cc99666acdc8300
Chad Clayton <chad@kcwaverider.com>
2015-06-15 09:04:28 -0500

    Assignment 11-loops completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,22 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSInteger smallNumber, largeNumber;
+    NSString *listOfNumbers = @"";
+    
+    if (number < otherNumber) {
+        smallNumber = number;
+        largeNumber = otherNumber;
+    } else {
+        smallNumber = otherNumber;
+        largeNumber = number;
+    }
+    
+    for (NSInteger n = smallNumber; n <= largeNumber; n++) {
+        listOfNumbers = [listOfNumbers stringByAppendingString:[@(n) stringValue]];
+    }
+    return listOfNumbers;
 }
 
 @end
