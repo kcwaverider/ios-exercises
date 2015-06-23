68f2e3e7c4885c8d7842756fafe70aa2f1d35ef4
Chad Clayton <chad@kcwaverider.com>
2015-06-22 23:54:35 -0500

    Completed assignment-14-dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,43 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    NSString *favoriteDrink = [NSString alloc];
+    if (characterDictionary[@"favorite drink"] != nil && [characterDictionary[@"favorite drink"] isKindOfClass:[NSString class]]) {
+        favoriteDrink = characterDictionary[@"favorite drink"];
+    } else {
+        favoriteDrink = nil;
+    }
+    
+    
+    return favoriteDrink;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *drinkMenu = [[NSMutableArray alloc] init];
+    NSString *favoriteDrink = [NSString alloc];
+    
+    for (int n = 0; n < charactersArray.count; n++) {
+        if (charactersArray[n][@"favorite drink"] != nil && [charactersArray[n][@"favorite drink"] isKindOfClass:[NSString class]]) {
+            favoriteDrink = charactersArray[n][@"favorite drink"];
+        } else {
+            favoriteDrink = nil;
+        }
+        
+        drinkMenu[n] = favoriteDrink;
+
+    }
+    return drinkMenu;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *quotedDictionary = [characterDictionary mutableCopy];
+    [quotedDictionary setObject:@"White men can't jump" forKey:@"quote" ];
+    
+    return quotedDictionary;
 }
 
 @end
