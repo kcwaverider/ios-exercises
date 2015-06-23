01b1fe9141cf8d0710eb956d6044b53a900ff4f7
Chad Clayton <chad@kcwaverider.com>
2015-06-23 07:20:37 -0500

    Finished Assignment 15 data types, hooray\!

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,48 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    NSNumber *newNumber = [NSNumber numberWithFloat:([number floatValue] *2)];
+    
+    return newNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSInteger low, high;
+    
+    if (number < otherNumber) {
+        low = number;
+        high = otherNumber;
+    } else {
+        low = otherNumber;
+        high = number;
+    }
+    
+    NSMutableArray *mutableArray = [NSMutableArray new];
+    
+    for (NSInteger n = 0; n <= high - low; n++) {
+        mutableArray[n] = [NSNumber numberWithInteger:(low + n)];
+    }
+    
+    NSArray *finalArray = [mutableArray copy];
+    
+    return finalArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSInteger lowest = [arrayOfNumbers[0] integerValue];
+    NSInteger tempNum;
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        
+        tempNum = [number integerValue];
+        if ( tempNum < lowest) {
+            lowest = tempNum;
+        }
+    }
+    
+    return lowest;
 }
 
 @end
